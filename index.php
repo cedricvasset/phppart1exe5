4b4d89addd8a7b0e83a0bbf36b67ab9236595540
cedricvasset <vasset.cedric@orange.fr>
2018-12-14 16:06:59 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php part1 ex5</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <p>
+    <?php
+    $int = NULL;
+    echo $int;
+    ?>
+  </p>
+  <p>
+    <?php
+    $int = 44;
+    echo $int;
+    ?>
+  </p>
+</body>
+</html>
